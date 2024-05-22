d7ebd425dd3932416e081df3bca16f5587277b98
avictorbabs <avictorbabs@gmail.com>
2024-05-22 06:54:33 +0100

    update

--- app/Filament/Resources/LoanResource.php
+++ app/Filament/Resources/LoanResource.php
@@ -20,7 +20,6 @@ class LoanResource extends Resource
 {
     use CreateRecord\Concerns\HasWizard;
 
-//    public $currentStep = 1;  // Default step
     protected static ?string $model = Loan::class;
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
     protected static ?string $modelLabel = 'Loan';
@@ -37,11 +36,11 @@ class LoanResource extends Resource
                         Wizard\Step::make('amount')->label('Loan Amount')
                         ->schema([
                             Forms\Components\TextInput::make('name'),
-                            Forms\Components\TextInput::make('tuition_fees')->numeric()->required()->debounce(1000),
-                            Forms\Components\TextInput::make('hostel_fees')->numeric()->required()->debounce(1000),
-                            Forms\Components\TextInput::make('other_costs')->numeric()->required()->debounce(1000),
-                            Forms\Components\TextInput::make('cost_of_living')->numeric()->required()->debounce(1000),
-                        ])->columns(columns:2)->reactive()
+                            Forms\Components\TextInput::make('tuition_fees')->numeric()->required(),
+                            Forms\Components\TextInput::make('hostel_fees')->numeric()->required(),
+                            Forms\Components\TextInput::make('other_costs')->numeric()->required(),
+                            Forms\Components\TextInput::make('cost_of_living')->numeric()->required(),
+                        ])->columns(columns:2)
                         ->afterStateUpdated(function ($state, $set, $get) {
                             // Calculate total loan amount when any field updates
                             $tuition_fees = floatval($get('tuition_fees') ?? 0);
@@ -55,31 +54,13 @@ class LoanResource extends Resource
                             // Set the computed value
                             $set('total_loan_amount', $totalLoanAmount);
                         }),
-
-
-                        // ->afterValidation(function ($livewire) {
-                        //     //if ($livewire->getCurrentStep() === 'amount') {
-                        //         try {
-                        //             $tuition_fees = floatval($livewire->form->getState()['tuition_fees'] ?? 0);
-                        //             $hostel_fees = floatval($livewire->form->getState()['hostel_fees'] ?? 0);
-                        //             $other_costs = floatval($livewire->form->getState()['other_costs'] ?? 0);
-                        //             $cost_of_living = floatval($livewire->form->getState()['cost_of_living'] ?? 0);
-
-                        //             $totalLoanAmount = $tuition_fees + $hostel_fees + $other_costs + $cost_of_living;
-                        //             $livewire->form->fill(['total_loan_amount' => $totalLoanAmount]);
-
-                        //             logger()->info('Total loan amount calculated: ', ['amount' => $totalLoanAmount]);
-                        //         } catch (\Exception $e) {
-                        //             logger()->error('Error in afterValidation: ' . $e->getMessage());
-                        //             throw $e; // Rethrow the exception to make it visible
-                        //         }
-                        //     //}
-                        // }),
-
                         Wizard\Step::make('tenure')->label('Loan Tenure')->schema([
-                            Forms\Components\TextInput::make('total_loan_amount')->numeric()->required()->default(fn ($get) => $get('total_loan_amount')),
+                            Forms\Components\TextInput::make('total_loan_amount')->numeric()->required()->default(fn ($get) => $get('total_loan_amount'))->editable('false'),
                             Forms\Components\TextInput::make('interest_rate')->numeric()->default('34')->hidden()->required(),
                             Forms\Components\Select::make('tenure_options')->label('Tenure Options (months)')->options([ 3 => '3 months', 6 => '6 months', 12 => '12 months', 18 => '18 months'])->required(),//->default(18),
+                            Forms\Components\TextInput::make('payable_interest_value')->hidden(),
+                            Forms\Components\TextInput::make('monthly_repayment')->hidden(),
+                            Forms\Components\TextInput::make('total_repayment')->hidden(),
                         ])->columns(columns:2),
                     ])
                 ])
